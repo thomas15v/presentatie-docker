ba20abe75deb00f3ec1e54a72cb1eefdaba12bf6
thomas15v <tvanmellaerts@gmail.com>
2017-01-21 21:56:18 +0100

    Removed something stuppid

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var revealRunInTerminal = require('reveal-run-in-terminal');
 var staticFile = require('connect-static-file');
 var app = express();
 
-app.use(revealRunInTerminal({'allowRemote': true}));
 app.use('/', express.static('node_modules/reveal.js'));
 app.use('/static', express.static('node_modules/typed.js'));
 app.use('/static/js', express.static('node_modules/jquery/dist'));
