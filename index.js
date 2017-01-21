6688dfdce2ef2c55400a9fcd3ebe0954c658e68c
thomas15v <tvanmellaerts@gmail.com>
2017-01-21 22:12:10 +0100

    slightly mad

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use('/static', express.static('node_modules/typed.js'));
 app.use('/static/js', express.static('node_modules/jquery/dist'));
 app.use('/static/js', express.static('node_modules/headjs/dist/1.0.0'));
 app.use('/static/css/highlight', express.static('node_modules/highlight.js/styles'))
+app.use('/', express.static('.'));
 
 app.listen(8080, function () {
     console.log("server started on port 8080")
