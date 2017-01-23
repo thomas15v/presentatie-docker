bb0dababd962c570bc234aecd58a74b3e22f2ecc
thomas15v <tvanmellaerts@gmail.com>
2017-01-23 09:00:40 +0100

    working farder on it

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var staticFile = require('connect-static-file');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var fs        = require('fs');
+var Mustache  = require('mustache');
 
 app.use('/', express.static('.'));
 app.use('/', express.static('node_modules/reveal.js'));
@@ -10,6 +14,39 @@ app.use('/static/js', express.static('node_modules/headjs/dist/1.0.0'));
 app.use('/static/css/highlight', express.static('node_modules/highlight.js/styles'))
 
 
-app.listen(8080, function () {
+io.on( 'connection', function( socket ) {
+    console.log("something happend")
+    socket.on( 'new-subscriber', function( data ) {
+        socket.broadcast.emit( 'new-subscriber', data );
+    });
+
+    socket.on( 'statechanged', function( data ) {
+        delete data.state.overview;
+        socket.broadcast.emit( 'statechanged', data );
+    });
+
+    socket.on( 'statechanged-speaker', function( data ) {
+        delete data.state.overview;
+        socket.broadcast.emit( 'statechanged-speaker', data );
+    });
+
+});
+
+app.get('/', function( req, res ) {
+    res.writeHead( 200, { 'Content-Type': 'text/html' } );
+    fs.createReadStream( opts.baseDir + '/index.html' ).pipe( res );
+});
+
+app.get( '/notes/:socketId', function( req, res ) {
+    fs.readFile('notes.html', function( err, data ) {
+        res.send( Mustache.to_html( data.toString(), {
+            socketId : req.params.socketId
+        }));
+    });
+
+});
+
+server.listen(8080, function () {
     console.log("server started on port 8080")
 });
+
